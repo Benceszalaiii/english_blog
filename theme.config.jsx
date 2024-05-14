0f91518b00a3657d74d3619fd04c3ed8f3c3a4d0
szbence2007 <szbence2007@gmail.com>
2024-05-14 11:09:13 +0000

    Imported site

---
+++ theme.config.jsx
@@ -0,0 +1,42 @@
+import Cusdis from 'nextra-theme-blog/cusdis'
+
+/* eslint sort-keys: error */
+export default {
+  comments: <Cusdis />,
+  components: {
+    h1: ({ children }) => (
+      <h1
+        style={{
+          WebkitBackgroundClip: 'text',
+          WebkitTextFillColor: 'transparent',
+          backgroundClip: 'text',
+          backgroundImage: 'linear-gradient(90deg,#7928CA,#FF0080)'
+        }}
+      >
+        {children}
+      </h1>
+    )
+  },
+  cusdis: {
+    appId: 'a2d11511-7012-4254-9483-cb49c8f4dfe8'
+  },
+  darkMode: true,
+  dateFormatter: date => `Last updated at ${date.toDateString()}`,
+  footer: (
+    <small style={{ display: 'block', marginTop: '8rem' }}>
+      {new Date().getFullYear()} © Bence Szalai
+      <style jsx>{`
+        a {
+          float: right;
+        }
+
+        @media screen and (max-width: 480px) {
+          article {
+            padding-top: 2rem;
+            padding-bottom: 4rem;
+          }
+        }
+      `}</style>
+    </small>
+  )
+}
